f9e3babbd532712937d38b0cce99ca9483317384
Kirill Byvshev <k@byv.sh>
2020-07-20 21:39:13 +0300

    echo incoming data back

--- src/main/resources/main.lua
+++ src/main/resources/main.lua
@@ -1,21 +1,9 @@
-local server_core = require("omgameserver.server_core")
 local udp_server = require("omgameserver.udp_server")
 
 local echo_server = {}
 
-function echo_server:core_tick(event)
-end
-
-function echo_server:udp_client_connected(event)
-end
-
-function echo_server:udp_client_disconnected(event)
-end
-
 function echo_server:udp_data_received(event)
+    udp_server.dispatch_udp_send_data_event(event.client_uid, event.data,true)
 end
 
-server_core.listen_core_tick_event_on(echo_server)
-udp_server.listen_udp_client_connected_event_on(echo_server)
-udp_server.listen_udp_client_disconnected_event_on(echo_server)
 udp_server.listen_udp_data_received_event_on(echo_server)
